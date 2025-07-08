64d417e3278950f4a25d5afd51a7aa0a9de95496
Rob Ray <214191491+KDZU-antisocial@users.noreply.github.com>
2025-07-08 13:45:56 -0700

    fix link to sitemap-0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kdzu.org',
@@ -23,7 +22,6 @@ export default defineConfig({
         VimeoVideo: './src/components/VimeoVideo.astro',
         AudioPlayer: './src/components/AudioPlayer.astro'
       }
-    }),
-    sitemap()
+    })
   ]
 });
